e5075f50de51b5c6697caa8be718e84a49831e8b
Alexandr Pilipenko <vopilipenko@gmail.com>
2025-01-27 19:51:17 +0200

    fix import

--- src/tests/unit/components/app-content-switcher/AppContentSwitcher.spec.jsx
+++ src/tests/unit/components/app-content-switcher/AppContentSwitcher.spec.jsx
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import AppContentSwitcher from '~/components/app-content-switcher/AppContentSwitcher'
-import { describe, it, expect, vi } from 'vitest'
+import { vi } from 'vitest'
 
 describe('AppContentSwitcher', () => {
   const switchOptionsMock = {
