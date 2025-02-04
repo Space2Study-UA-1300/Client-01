8173238668028398d1ac1fbe20a1105b8f955005
Oleh Senkiv <33096162+senkiv-oleh@users.noreply.github.com>
2025-02-04 18:45:29 +0200

    feature-82-stepper-category-section (#18)
    * Created Subject Step
    
    * Fixed style and location "Add one more subject" button
    
    * fixed some inscriptions
    
    * corrected image title

---
+++ src/containers/tutor-home-page/subjects-step/subject-form/SubjectForm.styles.js
@@ -0,0 +1,21 @@
+export const styles = {
+  form: {
+    display: 'flex',
+    flexDirection: 'column',
+    minWidth: { sm: '340px' }
+  },
+  button: {
+    fontWeight: 500,
+    fontSize: '16px',
+    lineHeight: '24px',
+    letterSpacing: '0.5px',
+    textTransform: 'initial',
+    backgroundColor: '#eceff1',
+    color: '#263238',
+    width: '100%',
+    boxShadow: 'none',
+    '&:hover': {
+      backgroundColor: '#eceff1'
+    }
+  }
+}
