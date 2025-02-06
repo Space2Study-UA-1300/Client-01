aeb003d986bc3435ea450bf16da2ee0561619f37
tetianasvidrak <svidrak.tetiana@gmail.com>
2025-02-06 19:33:39 +0100

    added search input for category page with request to BE

--- src/hooks/use-categories-names.tsx
+++ src/hooks/use-categories-names.tsx
@@ -3,20 +3,37 @@ import { defaultResponses } from '~/constants'
 
 import useAxios from '~/hooks/use-axios'
 import { categoryService } from '~/services/category-service'
-import { CategoryNameInterface } from '~/types'
+import { CategoryNameInterface, ErrorResponse } from '~/types'
 
-const useCategoriesNames = ({ fetchOnMount = true } = {}) => {
+interface UseCategoriesNamesProps<T> {
+  fetchOnMount?: boolean
+  transform?: (data: CategoryNameInterface[]) => T[]
+}
+interface UseCategoriesNamesResult<T> {
+  loading: boolean
+  response: T[]
+  fetchData: () => Promise<void>
+  error: ErrorResponse | null
+}
+
+const useCategoriesNames = <T = CategoryNameInterface,>({
+  fetchOnMount = true,
+  transform
+}: UseCategoriesNamesProps<T>): UseCategoriesNamesResult<T> => {
   const getCategoriesNames = useCallback(
     () => categoryService.getCategoriesNames(),
     []
   )
 
   const { loading, response, fetchData, error } = useAxios<
-    CategoryNameInterface[]
+    CategoryNameInterface[],
+    undefined,
+    T[]
   >({
     service: getCategoriesNames,
     fetchOnMount,
-    defaultResponse: defaultResponses.array
+    defaultResponse: defaultResponses.array,
+    transform
   })
 
   return { loading, response, fetchData, error }
