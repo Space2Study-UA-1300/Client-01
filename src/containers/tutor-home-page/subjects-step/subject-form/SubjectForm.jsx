9b56bfa2bf1ad197d6d12a733b0a6f3e4b7226b5
Oleh Senkiv <o.senkiv.v@gmail.com>
2025-02-10 22:39:34 +0200

    changed <AppAutoComplete> logic for Category and Subject fields

--- src/containers/tutor-home-page/subjects-step/subject-form/SubjectForm.jsx
+++ src/containers/tutor-home-page/subjects-step/subject-form/SubjectForm.jsx
@@ -7,7 +7,6 @@ import Typography from '@mui/material/Typography'
 
 import { styles } from './SubjectForm.styles'
 
-import AppSelect from '~/components/app-select/AppSelect'
 import AppAutoComplete from '~/components/app-auto-complete/AppAutoComplete'
 import AppButton from '~/components/app-button/AppButton'
 import AppChipList from '~/components/app-chips-list/AppChipList'
@@ -20,6 +19,7 @@ const SubjectForm = ({ handleSubmit }) => {
   const { t } = useTranslation()
 
   const [inputCategory, setInputCategory] = useState('')
+  const [inputSubject, setInputSubject] = useState('')
   const [selectedCategory, setSelectedCategory] = useState('')
   const [selectedSubject, setSelectedSubject] = useState('')
   const [subjectsChipList, setSubjectsChipList] = useState([
@@ -31,20 +31,30 @@ const SubjectForm = ({ handleSubmit }) => {
   }, [subjectsChipList, stepData])
 
   const categoryArray = () => {
+    return subjectsMock.map(({ category }) => ({ label: category }))
+  }
+
+  const subjectArray = () => {
     const newArray = []
+
     subjectsMock.map(({ category, subjects }) => {
       subjects.map(({ name }) =>
-        newArray.push({
-          lable: category,
-          subject: name
-        })
+        selectedCategory
+          ? category === selectedCategory &&
+            newArray.push({
+              label: category,
+              subject: name
+            })
+          : newArray.push({
+              label: category,
+              subject: name
+            })
       )
     })
 
     return newArray.sort((a, b) => {
       let firstSubject = a.subject.toLowerCase()
       let secondSubject = b.subject.toLowerCase()
-
       return firstSubject < secondSubject
         ? -1
         : firstSubject > secondSubject
@@ -53,33 +63,29 @@ const SubjectForm = ({ handleSubmit }) => {
     })
   }
 
-  const getCategorySubjects = subjectsMock.find(({ category }) => {
-    return category.toLowerCase() === selectedCategory.toLocaleLowerCase()
-  })
-
-  const subjectFields = (value = []) =>
-    value.map(({ name }) => {
-      return {
-        title: name,
-        value: name
-      }
-    })
-
-  const onInputChangeCategory = (_, value) => {
-    setSelectedSubject('')
-    setSelectedCategory('')
-    setInputCategory(value)
-  }
-
-  const handleAutoCompleteChangeCategory = (_, value) => {
-    if (value) {
-      setSelectedCategory(value.lable)
-      setSelectedSubject(value.subject)
+  const onInputChangeCategory = (_, value, reason) => {
+    if (reason === 'clear') {
+      formReset()
+    } else {
+      setInputCategory(value)
+      setSelectedSubject('')
+      setInputSubject('')
     }
   }
 
-  const handleSelectSubject = (event) => {
-    setSelectedSubject(event)
+  const onInputChangeSubject = (_, value, reason) => {
+    if (reason !== 'reset') {
+      setInputSubject(value)
+    }
+  }
+
+  const handleSelect = (_, value) => {
+    if (value) {
+      setSelectedCategory(value.label || '')
+      setInputCategory(value.label || '')
+      setSelectedSubject(value.subject || '')
+      setInputSubject(value.subject || '')
+    }
   }
 
   const handleDelete = (chipToDelete) => {
@@ -89,35 +95,31 @@ const SubjectForm = ({ handleSubmit }) => {
     setSubjectsChipList(() => filteredSubject)
   }
 
+  const formReset = () => {
+    setSelectedCategory('')
+    setSelectedSubject('')
+    setInputCategory('')
+    setInputSubject('')
+  }
+
   const addCategory = () => {
-    if (selectedSubject) {
+    if (selectedSubject && selectedCategory) {
       if (!subjectsChipList.includes(selectedSubject)) {
         setSubjectsChipList(() => [...subjectsChipList, selectedSubject])
       }
-      setSelectedCategory('')
-      setSelectedSubject('')
-      setInputCategory('')
+      formReset()
     }
   }
 
-  const renderOptions = (props, { lable, subject }) => {
+  const renderOptions = (props, { label, subject }) => {
     const { ...optionProps } = props
     return (
       <Box component='li' sx={{ height: '48px' }} {...optionProps}>
         {subject}
         <Typography sx={{ color: 'grey', ml: '10px' }}>
-          Category: {lable}
+          Category: {label}
         </Typography>
       </Box>
-    )
-  }
-
-  const filterOptions = (options, state) => {
-    const filterValue = state.inputValue.toLowerCase()
-    return options.filter(
-      (option) =>
-        option.subject.toLowerCase().includes(filterValue) ||
-        option.lable.toLowerCase().includes(filterValue)
     )
   }
 
@@ -125,33 +127,52 @@ const SubjectForm = ({ handleSubmit }) => {
     <Box component='form' onSubmit={handleSubmit} sx={styles.form}>
       <AppAutoComplete
         disableClearable={false}
-        filterOptions={filterOptions}
+        filterOptions={(options) =>
+          options.filter((option) =>
+            option.label.toLowerCase().includes(inputCategory.toLowerCase())
+          )
+        }
         freeSolo
-        getOptionLabel={(option) => option?.lable ?? ''}
+        getOptionLabel={(option) => option?.label ?? ''}
         inputValue={inputCategory}
-        isOptionEqualToValue={(option, value) => option?.lable === value?.lable}
-        onChange={handleAutoCompleteChangeCategory}
+        isOptionEqualToValue={(option, value) =>
+          option?.label === value?.label || value === null
+        }
+        onChange={handleSelect}
         onInputChange={onInputChangeCategory}
         options={categoryArray()}
-        renderOption={(props, option) => renderOptions(props, option)}
         sx={{ mb: '20px' }}
         textFieldProps={{
           label: t('becomeTutor.categories.mainSubjectsLabel')
         }}
-        value={
-          categoryArray().find((option) => option.lable === selectedCategory) ||
-          null
+        value={inputCategory ? { label: inputCategory } : null}
+      />
+      <AppAutoComplete
+        disableClearable={false}
+        filterOptions={(options) =>
+          options.filter(
+            (option) =>
+              option.label.toLowerCase().includes(inputSubject.toLowerCase()) ||
+              option.subject.toLowerCase().includes(inputSubject.toLowerCase())
+          )
         }
+        freeSolo
+        getOptionLabel={(option) => option?.subject ?? ''}
+        inputValue={inputSubject}
+        isOptionEqualToValue={(option, value) =>
+          option?.subject === value?.subject || value === null
+        }
+        onChange={handleSelect}
+        onInputChange={onInputChangeSubject}
+        options={subjectArray()}
+        renderOption={(props, option) => renderOptions(props, option)}
+        sx={{ mb: '16px' }}
+        textFieldProps={{
+          label: t('becomeTutor.categories.subjectLabel')
+        }}
+        value={inputSubject}
       />
-      <AppSelect
-        fields={subjectFields(
-          selectedCategory ? getCategorySubjects.subjects : []
-        )}
-        label={t('becomeTutor.categories.subjectLabel')}
-        setValue={handleSelectSubject}
-        sx={(styles.selectField, { mb: '16px' })}
-        value={selectedSubject}
-      />
+
       <AppButton onClick={addCategory} sx={styles.button}>
         {t('becomeTutor.categories.btnText')}
       </AppButton>
