161a6517d3a411cf4b5aeaa7caf570e3d4c04da6
Tamila57 <156967484+Tamila57@users.noreply.github.com>
2025-02-10 18:37:42 +0200

    implement General info step (#25)
    feature 74 add General info step

---
+++ src/containers/tutor-home-page/general-info-step/constantsGeneral.js
@@ -0,0 +1,6 @@
+export const COUNTRIES_AND_CITIES = {
+  Ukraine: ['Kyiv', 'Lviv', 'Odessa', 'Kharkiv'],
+  USA: ['New York', 'Los Angeles', 'Chicago', 'Miami'],
+  Germany: ['Berlin', 'Munich', 'Hamburg', 'Frankfurt'],
+  France: ['Paris', 'Lyon', 'Marseille', 'Nice']
+}
