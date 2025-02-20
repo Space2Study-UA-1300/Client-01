23d2d9c68a105b152883db5e4815d0a179f9cad0
Arsen <arsen.danylenko@gmail.com>
2025-02-20 05:42:56 -0600

    added rendering of user photo on profile avatar

--- src/components/profile-picture/ProfilePicture.jsx
+++ src/components/profile-picture/ProfilePicture.jsx
@@ -1,19 +1,25 @@
 import { useSelector } from 'react-redux'
-import { Box, Typography } from '@mui/material'
+import { Box, Typography, Alert, Snackbar } from '@mui/material'
 import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined'
 import { styles } from '~/containers/navigation-icons/NavigationIcons.styles'
 
 import { userService } from '~/services/user-service'
 import useAxios from '~/hooks/use-axios'
-import { useEffect, useCallback } from 'react'
+import { useCallback, useState } from 'react'
 
 const ProfilePicture = () => {
   const userId = useSelector((state) => state.appMain.userId)
   const userRole = useSelector((state) => state.appMain.userRole)
+  const [errorAlert, setErrorAlert] = useState(null)
+  const [open, setOpen] = useState(false)
 
   const { getUserById } = userService
 
-  const { response: userData, error } = useAxios({
+  const {
+    loading,
+    response: userData,
+    error
+  } = useAxios({
     service: useCallback(
       () => getUserById(userId, userRole),
       [userId, userRole]
@@ -22,24 +28,37 @@ const ProfilePicture = () => {
     fetchOnMount: !!userId
   })
 
-  useEffect(() => console.log(userData), [userData])
-
-  if (!userData) {
+  if (!userData || loading) {
     return <AccountCircleOutlinedIcon />
   }
 
   if (!userId) {
-    return <Typography>User not authenticated</Typography>
+    setErrorAlert('User not authenticated')
+    setOpen(true)
+    return <AccountCircleOutlinedIcon />
   }
 
   if (error) {
-    return <Typography>Error loading profile</Typography>
+    setErrorAlert(error.message)
+    setOpen(true)
+    return <AccountCircleOutlinedIcon />
   }
 
   return (
     <Box sx={styles.profilePicture}>
-      {userData?.profilePicture ? (
-        <img alt='Profile' src={userData.profilePicture} />
+      <Snackbar
+        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+        autoHideDuration={5000}
+        onClose={() => setOpen(false)}
+        open={open}
+      >
+        <Alert onClose={() => setOpen(false)} severity='error'>
+          {errorAlert}
+        </Alert>
+      </Snackbar>
+
+      {userData.photo[0].url ? (
+        <img alt='Profile' src={userData.photo[0].url} />
       ) : (
         <Box sx={styles.profilePictureInitials}>
           <Typography variant='h6'>
